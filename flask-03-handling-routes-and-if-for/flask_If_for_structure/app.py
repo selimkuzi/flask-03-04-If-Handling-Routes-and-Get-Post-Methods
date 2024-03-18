f67643825edda9a021d00dd83287b6662bc8654a
selim <selimkuzi@gmail.com>
2024-03-18 22:49:30 +0300

    second

--- flask-03-handling-routes-and-if-for/flask_If_for_structure/app.py
+++ flask-03-handling-routes-and-if-for/flask_If_for_structure/app.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 @app.route("/")
 def head():
     first="This is my first conditions experience"
-    return render_template("index.html")
+    return render_template("index.html", message= "merhaba dunyali")
 
 
 # Create a function named header which prints numbers elements of list one by one in `index.html` 
 # and assign to the route of ('/')
 @app.route("/list")
 def header():
-    names =["tom", "osvaldo", "guile", "tommy"]
+    names =["tom", "selim", "guile", "tommy"]
     # numbers = range (1,11)
     return render_template("body.html", object = names)
 
